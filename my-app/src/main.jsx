1de27f136f2382f604313402ca82a670e120a43f
kabeloChuene <kabelochuene90@gmail.com>
2026-04-20 15:07:23 +0200

    Update main.jsx

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,16 +1,14 @@
 import './style.css'
 import { setupCounter } from './counter.jsx'
 import { createRouter } from './router.jsx'
-import { homePage } from './pages/home.jsx'
 import { dashboardPage } from './pages/dashboard.jsx'
 import { loginPage } from './pages/login.jsx'
 import { registrationPage } from './pages/registration.jsx'
 
 const routes = {
-  '/': homePage,
+  '/': registrationPage,
   '/dashboard': dashboardPage,
   '/login': loginPage,
-  '/registration': registrationPage,
 }
 
 const appRoot = document.querySelector('#app')
